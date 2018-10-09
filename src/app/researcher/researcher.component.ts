43e07c73ef09b816e0322f071c9bbdd99a2d37bf
Ronak <ronak.scriptbees.com>
2018-10-09 14:09:51 +0530

    modal

--- src/app/researcher/researcher.component.ts
+++ src/app/researcher/researcher.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import{ResearchdataService} from '../services/research.service';
+declare let require: any;
+
+declare var $: any;
 @Component({
   selector: 'app-researcher',
   templateUrl: './researcher.component.html',
@@ -7,6 +10,8 @@ import{ResearchdataService} from '../services/research.service';
 })
 export class ResearcherComponent implements OnInit {
   data=[];
+
+  private reseachViewData: Object = {};
   constructor(private researchdataService:ResearchdataService) { }
 
   ngOnInit() {
@@ -18,11 +23,18 @@ export class ResearcherComponent implements OnInit {
               console.log("---data",this.data)
             });       
         }
-
+closemodal(){
+  $('#myModal').hide();
+}
 
         openfullmode(data){
-           alert("click")
+           $('#myModal').show();
            console.log("----------full view data view",data)
+            this.reseachViewData['authorname']=data.author;
+            this.reseachViewData['created_date']=data.created_date
+            this.reseachViewData['result_abstract']=data.result_abstract
+            this.reseachViewData['result_title']=data.result_title
+            this.reseachViewData['search_term']=data.search_term
 
         }
   
